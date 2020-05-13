37f4c5c74f2a67749684603c5b70cba0105869f8
hylerrix <hylerrix@gmail.com>
2020-05-13 12:38:29 +0800

    feat: init demos folder
    Signed-off-by: hylerrix <hylerrix@gmail.com>

---
+++ demos/welcome.ts
@@ -0,0 +1,6 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
